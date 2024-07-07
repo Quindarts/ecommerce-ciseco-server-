99c3ef22c7b54ba2b09cf8e2a77dbd9eaa9bc79c
Quindarts <quang82thcspb@gmail.com>
2024-07-07 23:42:04 +0700

    fix

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,5 +1,5 @@
 const ROUTE = {
-    AUTH: '/api/v1/auth',
+    AUTH: '/auth',
     LOGIN: '/login',
     REGISTER: '/register',
     CHANGE_PASSWORD: '/change',
@@ -7,11 +7,11 @@ const ROUTE = {
     GENERATE_ACCESSTOKEN: '/accessToken-generate',
 
     //Address
-    ADDRESS: '/api/v1/addresses',
+    ADDRESS: '/addresses',
     GET_ADDRESS_BY_ID: '/:address_id',
 
     //Category
-    CATEGORY: '/api/v1/categories',
+    CATEGORY: '/categories',
     CATEGORY_BY_ID: '/:category_id',
     DELETE_CATEGORY_BY_ID: '/:category_id',
     CATEGORY_CHILD_BY_PATH_PARENT: '/children/:path',
@@ -21,11 +21,11 @@ const ROUTE = {
     CATEGORY_QUERY: '/search',
 
     //Category
-    PROVIDER: '/api/v1/providers',
+    PROVIDER: '/providers',
     PROVIDER_BY_ID: '/:provider_id',
 
     //Product
-    PRODUCT: '/api/v1/products',
+    PRODUCT: '/products',
     PRODUCT_ADMIN: '/admin',
     PRODUCT_BY_ID: '/:product_id',
     PRODUCT_BY_ID_ATTRIBUTE_PRODUCT_ID:
@@ -36,34 +36,36 @@ const ROUTE = {
     PRODUCT_QUERY_BY_CATEGORY: '/category/:category_id',
 
     //Product liked
-    PRODUCT_LIKED: '/api/v1/productLikeds',
+    PRODUCT_LIKED: '/productLikeds',
     PRODUCT_LIKED_USER: '/:user_id',
 
     // PRODUCT_LIKED_DELETE
 
     //AttributeProduct
-    ATTRIBUTE_PRODUCT: '/api/v1/attributeProducts',
+    ATTRIBUTE_PRODUCT: '/attributeProducts',
     ATTRIBUTE_PRODUCT_BY_ID: '/:attribute_product_id',
+    ATTRIBUTE_PRODUCT_FILTER: '/:',
+    ATTRIBUTE_PRODUCT_QUERY: '/:',
 
     //User
-    USER: '/api/v1/users',
+    USER: '/users',
     USER_BY_ID: '/:user_id',
     USER_CHANGE_ACTIVE: '/change-active/:user_id',
 
     //Cart
-    CART: '/api/v1/carts',
+    CART: '/carts',
     CART_BY_USER_ID: '/:user_id',
     CART_UPDATE_BY_PRODUCT_ID: '/:user_id/products/:product_id',
-    // CART_UPDATE_BY_PRODUCT_ID: '/api/v1/:user_id/product/:product_id',
+    // CART_UPDATE_BY_PRODUCT_ID: '/:user_id/product/:product_id',
 
     //Order
-    ORDER: '/api/v1/orders',
+    ORDER: '/orders',
     ORDER_BY_ID: '/:order_id',
     ORDER_BY_PARAMS: '/params',
     ORDER_BY_USER_ID: '/user/:user_id',
     ORDER_BY_ID_CREATE_PAYMENT_ONLINE: '/:order_id/payment-online',
 
-    PAYMENT: '/api/v1/payment',
+    PAYMENT: '/payment',
     CREATE_PAYMENT: '/create-payment-url',
     GET_PAYMENT_ONLINE: '/payment-success',
 }
